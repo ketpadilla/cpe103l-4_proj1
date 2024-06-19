6f4037b77047b9d922c1c08dd2df59a02cfeee4f
Kristian Padilla <ketpadilla05@gmail.com>
2024-06-19 12:54:53 +0800

    create main program

---
+++ main.cpp
@@ -0,0 +1,30 @@
+// * CPE103L-4 PROJECT 1 - COLLABORATIVE PROGRAMMING
+/* 
+  Group ___
+  PADILLA, Kristian Emmanuel T.
+  ___
+  ___
+  ___
+  ___
+*/
+
+// Header files and namespace
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+// prototype functions
+
+
+
+// global variables
+
+
+
+// * main
+int main() {
+  return 0;
+}
+
+
